44b3286228df19eecfdd4265ea13776ce265d24f
saif76r <apnar-github-email@example.com>
2026-08-08 11:33:56 +0600

    Initial commit

--- src/data/defaultProfile.ts
+++ src/data/defaultProfile.ts
@@ -1,22 +1,22 @@
 import { ProfileData } from '../types';
 
 import logoImg from '../assets/images/bgi_logo_1786128343361.jpg';
-import profileImg from '../assets/images/hr_head.jpg';
+import profileImg from '../assets/images/mn_asshead.jpg';
 
 export const BGI_LOGO_PATH = logoImg;
 
 export const DEFAULT_PROFILE: ProfileData = {
-    name: 'Asma Sultana Minan ',
+    name: 'Anika Rahman  ',
 
-    dept: 'Human Resources',
+    dept: 'Management',
 
-    role: 'Director ',
+    role: 'Assistant Director',
 
-    email: 'minan.bgicommunity@gmail.com',
+    email: 'anikarahman.bgicommunity@gmail.com',
 
-    phone: '+880  1850150548',
+    phone: '+880  1975658359',
 
-    bloodGroup: 'O+',
+    bloodGroup: 'A+',
 
     avatarUrl: profileImg,
 
@@ -27,7 +27,7 @@ export const DEFAULT_PROFILE: ProfileData = {
     location: 'Dhaka, Bangladesh',
 
     socials: {
-        facebook: 'https://www.facebook.com/asma.sultana.minan',
+        facebook: 'https://www.facebook.com/profile.php?id=100056025837102',
 
        
     }
